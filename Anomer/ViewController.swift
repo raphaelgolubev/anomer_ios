7f3ee6853bb28a2386eb534b9509864fa1c74724
Raphael Golubev <raphael.golubev@gmail.com>
2025-07-22 16:56:10 +0300

    initial commit

---
+++ Anomer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anomer
+//
+//  Created by Рафаэль Голубев on 22.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
