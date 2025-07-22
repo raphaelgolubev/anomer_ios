fae928013cef24b07bfaa7bf31583caa9e0f33be
Raphael Golubev <raphael.golubev@gmail.com>
2025-07-22 17:39:16 +0300

    common project setup without storyboards

--- Anomer/ViewController.swift
+++ Anomer/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var greetingsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemBlue
+        label.font = .systemFont(ofSize: 24)
+        label.text = "Hello, world!"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(greetingsLabel)
+        NSLayoutConstraint.activate([
+            greetingsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            greetingsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            greetingsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
     }
-
-
 }
 
