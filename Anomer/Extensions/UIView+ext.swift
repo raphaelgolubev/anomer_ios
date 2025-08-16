7005aa80a0983efaf092056dd821fb54ab7dc1de
Raphael Golubev <raphael.golubev@gmail.com>
2025-08-16 23:34:59 +0300

    added UIView extensions

--- Anomer/Extensions/UIView+ext.swift
+++ Anomer/Extensions/UIView+ext.swift
@@ -5,4 +5,29 @@
 //  Created by Рафаэль Голубев on 16.08.2025.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - Accessibility
+extension UIView {
+    @discardableResult
+    func setAccessibilityLabel(to label: String) -> String {
+        self.accessibilityLabel = label
+        return label
+    }
+    
+    @discardableResult
+    func getAccessibilityLabel(default label: String) -> String {
+        if self.accessibilityLabel == nil {
+            return label
+        }
+        return self.accessibilityLabel!
+    }
+}
+
+// MARK: - Custom inits
+extension UIView {
+    convenience init(label: String) {
+        self.init()
+        self.setAccessibilityLabel(to: label)
+    }
+}
