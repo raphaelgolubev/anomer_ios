602c65d43608c675a520ccf129bab4bf6658262a
Raphael Golubev <raphael.golubev@gmail.com>
2025-08-16 23:34:45 +0300

    added preview canvas helper

--- Anomer/Helpers/Previews.swift
+++ Anomer/Helpers/Previews.swift
@@ -5,4 +5,85 @@
 //  Created by Рафаэль Голубев on 08.08.2025.
 //
 
-import Foundation
+// MARK: - UIView
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+/// Позволяет просматривать `UIView` в SwiftUI_Previews
+/// - Чтобы включить/выключить окно предпросмотра: `Xcode -> Editor -> Canvas`
+///
+/// Использование:
+/// ```
+/// final class MyView: UIView {
+///     //...
+/// }
+///
+/// // Поместите этот код в top-level файла
+/// struct MyView_Previews: PreviewProvider {
+///     static var previews: some View {
+///         UIViewPreview {
+///             MyView()
+///         }
+///     }
+/// }
+/// ```
+struct UIViewPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+    
+    init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+    
+    // MARK: UIViewRepresentable
+    func makeUIView(context: Context) -> UIView {
+        return view
+    }
+    
+    func updateUIView(_ view: UIView, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+
+#endif
+
+// MARK: - UIViewController
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+/// Позволяет просматривать `UIViewController` в SwiftUI_Previews
+/// - Чтобы включить/выключить окно предпросмотра: `Xcode -> Editor -> Canvas`
+///
+/// Использование:
+/// ```
+/// final class MyViewController: UIViewController {
+///     //...
+/// }
+///
+/// // Поместите этот код в top-level файла
+/// struct MyViewController_Previews: PreviewProvider {
+///     static var previews: some View {
+///         UIViewControllerPreview {
+///             MyViewController()
+///         }
+///     }
+/// }
+/// ```
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+
+    let viewController: ViewController
+
+    init(_ builder: @escaping () -> ViewController) {
+        viewController = builder()
+    }
+
+    // MARK: - UIViewControllerRepresentable
+    func makeUIViewController(context: Context) -> ViewController {
+        viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
+}
+#endif
